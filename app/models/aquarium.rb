add13421429be83a1daa9508d230c058f3181615
st8140 <74239438+st8140@users.noreply.github.com>
2021-08-03 18:03:54 +0900

    aquarium model spec作成

--- app/models/aquarium.rb
+++ app/models/aquarium.rb
@@ -2,7 +2,7 @@ class Aquarium < ApplicationRecord
   belongs_to :user
 
   validates :aquarium_introduction, presence: true, length: {maximum: 50}       
-  validates :aquarium_image, presence: { message: 'を選択して下さい。' }
+  validates :aquarium_image, presence: { message: 'を選択してください' }
 
   def user
     return User.find_by(id: self.user_id)
