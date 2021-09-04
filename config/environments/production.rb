27a97e7018af3effd0464bf256c7f769e6993e33
st8140 <74239438+st8140@users.noreply.github.com>
2021-09-04 15:23:21 +0900

    メール設定編集

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,17 +66,16 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
 
-  # config.action_mailer.delivery_method = :smtp
-  # host = 'https://portfolio-toyama.herokuapp.com/'
-  # config.action_mailer.default_url_options = { host: host }
-  # ActionMailer::Base.smtp_settings = {
-  #   :port           => ENV['MAILGUN_SMTP_PORT'],
-  #   :address        => ENV['MAILGUN_SMTP_SERVER'],
-  #   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  #   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  #   :domain         => host,
-  #   :authentication => :plain,
-  # }
+  config.action_mailer.default_url_options = {  :host => 'http://portfolio-toyama.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:"smtp.gmail.com",
+    domain: 'gmail.com',
+    port:587,
+    :user_name => ENV['GOOGLE_MAIL_ADDRESS'],
+    :password => ENV['GOOGLE_MAILER_PASSWORD'],
+    authentication: :login
+  }
     
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
